c8c4cf956b5390c28287217dcfd40b91635c3a0f
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2020-05-17 17:39:00 +0530

    from_hex method added

--- color.py
+++ color.py
@@ -2,4 +2,11 @@ from vector import Vector
 
 
 class Color(Vector):
-    pass
+    """Stores the RGB triplet values. Alias of Vector"""
+    @classmethod
+    def from_hex(cls, hex_color):
+        x = int(hex_color[1:3], 16) / 256.0
+        y = int(hex_color[3:5], 16) / 256.0
+        z = int(hex_color[5:7], 16) / 256.0
+
+        return cls(x, y, z)
